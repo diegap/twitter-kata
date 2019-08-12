693c98a5731209cfc367219d367769c9cb96f970
diego <diego@MacBook-Pro-de-soporte.local>
2019-08-12 17:16:18 -0300

    Cleanup Follow action

--- src/main/kotlin/katas/twitter/actions/FollowUser.kt
+++ src/main/kotlin/katas/twitter/actions/FollowUser.kt
@@ -4,10 +4,11 @@ import katas.twitter.model.user.Nickname
 import katas.twitter.repositories.UserRepository
 
 class FollowUser(private val userRepository: UserRepository) {
-    fun execute(user: Nickname, follower: Nickname) {
+    fun execute(user: Nickname, follow: Nickname) {
        userRepository.find(user).map {
-           val follow = it.copy(follows = it.follows.plusElement(follower))
-           userRepository.save(follow)
+           it.copy(follows = it.follows.plusElement(follow)).let { userToUpdate ->
+               userRepository.save(userToUpdate)
+           }
        }
     }
 }
